8573dfecb498cbeadb83f15e2f7e74e68a776b4e
niloufarnouri <78758381+niloufarnouri@users.noreply.github.com>
2022-05-01 16:04:14 -0400

    Add files via upload

---
+++ WebSearch_Google.py
@@ -0,0 +1,22 @@
+import webbrowser
+
+new=2
+
+tabUrl="http://google.com/search?q=";
+
+term= input("Enter search query: ");
+
+webbrowser.open(tabUrl+term,new=new);
+
+
+UPDATED SCRIPT:
+******************
+import webbrowser;
+
+new=2;
+
+tabUrl="http://google.com/search?q=";
+
+term= input("Enter search query: ");
+
+webbrowser.open(tabUrl+term,new=new);​
